fee96a82d3d5894395cc8eb9a037a3e05b99e3bf
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-10-03 21:24:51 -0400

    add mutex_cond_wait

--- lab4.2/Dining_Philosophers.c
+++ lab4.2/Dining_Philosophers.c
@@ -18,10 +18,12 @@
 
 pthread_t philosophers[N_PHILOSOPHERS];
 pthread_mutex_t forks[N_FORKS];
+pthread_cond_t cond_vars[N_FORKS];
 
 void *thread_fn(void *param);
 void pickup_forks(int philosopher_number);
 void return_forks(int philosopher_number);
+void eating_cycle(int n);
 
 int main(void) {
 	int i, k, j;
@@ -36,25 +38,17 @@ int main(void) {
 		}
 	}
 
-	//Create threads
-	printf("Creating threads\n");
-	for (i = 0; i < N_PHILOSOPHERS; i++) {
-		k = pthread_create(&philosophers[i], NULL, &thread_fn, (void *) i);
+	//Initializa condition variables
+	printf("Creating condition variables\n");
+	for (i = 0; i < N_FORKS; i++) {
+		k = pthread_cond_init(&cond_vars[i], NULL);
 		if (k == -1) {
-			printf("Pthread create failed...");
+			printf("Mutex init failed...");
 			exit(1);
 		}
 	}
 
-	//Join threads
-	printf("Joining threads\n");
-	for (j = 0; j < N_PHILOSOPHERS; j++) {
-		k = pthread_join(philosophers[j], NULL);
-		if (k != 0) {
-			printf("Pthread join failed...");
-			exit(1);
-		}
-	}
+	eating_cycle(4);
 
 	//Destroy mutexes
 	printf("Destroying mutexes\n");
@@ -70,7 +64,7 @@ int main(void) {
 }
 
 void *thread_fn(void *param) {
-	int pn =  (int) param;
+	int pn = (int) param;
 	printf("Philosopher %d is thinking...\n", pn);
 	sleep(1);
 	pickup_forks(pn);
@@ -79,9 +73,40 @@ void *thread_fn(void *param) {
 	return 0;
 }
 
+void eating_cycle(int n) {
+	int i, j, k, index;
+	for (index = 0; index < n; index++) {
+		printf("Cycle %d\n", index);
+		//Create threads
+		printf("Creating threads\n");
+		for (i = 0; i < N_PHILOSOPHERS; i++) {
+			k = pthread_create(&philosophers[i], NULL, &thread_fn, (void *) i);
+			if (k == -1) {
+				printf("Pthread create failed...");
+				exit(1);
+			}
+		}
+
+		//Join threads
+		printf("Joining threads\n");
+		for (j = 0; j < N_PHILOSOPHERS; j++) {
+			k = pthread_join(philosophers[j], NULL);
+			if (k != 0) {
+				printf("Pthread join failed...");
+				exit(1);
+			}
+		}
+
+	}
+}
+
 void pickup_forks(int philosopher_number) {
 	pthread_mutex_lock(&forks[philosopher_number]);
-	pthread_mutex_lock(&forks[(philosopher_number + 1) % N_FORKS]);
+	while (pthread_mutex_trylock(&forks[(philosopher_number + 1) % N_FORKS])
+			!= 0) {
+		pthread_cond_wait(&forks[philosopher_number],
+				&cond_vars[(philosopher_number + 1) % N_FORKS]);
+	}
 	printf("Philosopher %d is eating\n", philosopher_number);
 }
 
@@ -90,3 +115,4 @@ void return_forks(int philosopher_number) {
 	pthread_mutex_unlock(&forks[(philosopher_number + 1) % N_FORKS]);
 	printf("Philosopher %d finished eating\n", philosopher_number);
 }
+
